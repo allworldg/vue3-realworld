65afb6201f6eeb70633cda498ca39696879b06c0
allworldg <944937881@qq.com>
2025-04-25 10:11:48 +0800

    feat(axios):add axios,init vite base config,test to backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:'/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: "http://computer:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
+  }
 })
