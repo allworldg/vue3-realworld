7cfdaa983829d8afe38469d63ed5cb9e29ab36e1
allworldg <944937881@qq.com>
2025-05-14 16:51:01 +0800

    add typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,26 @@
 import pluginVue from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
 import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import tseslint from "typescript-eslint";
 export default [
   // add more generic rulesets here, such as:
   // js.configs.recommended,
   ...pluginVue.configs["flat/recommended"],
+  ...tseslint.config(tseslint.configs.recommended),
   // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
   {
     rules: {
       // override/add rules settings here, such as:
       // 'vue/no-unused-vars': 'error'
       "vue/multi-word-component-names": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
     },
     languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
       sourceType: "module",
       globals: {
         ...globals.browser,
