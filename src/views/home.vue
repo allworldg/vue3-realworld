c208edbc8450b325fe968270f06067f742738830
allworldg <944937881@qq.com>
2025-06-12 15:58:36 +0800

    add banner

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="">
     <div
-      class="bg-[#5cb85c] shadow-[0px_8px_8px_-8px_inset_rgb(0,0,0,0.3),0px_-8px_8px_-8px_inset_rgb(0,0,0,0.3)]"
+      class="bg-[#5cb85c] shadow-[0px_8px_8px_-8px_inset_rgb(0,0,0,0.3),0px_-8px_8px_-8px_inset_rgb(0,0,0,0.3)] mb-2"
     >
-      this is banner
-      <div>
-        <h1>Conduit</h1>
-        <p>A place to share your knowledge.</p>
+      <div class="flex flex-col py-8">
+        <h1
+          class="font-[Titillium_Web,sans-serif] text-5xl font-bold text-white mx-auto"
+        >
+          conduit
+        </h1>
+        <p class="text-2xl font-[SourceSans] text-white mx-auto my-3 w">
+          A place to share your knowledge.
+        </p>
       </div>
     </div>
+    <div class=""/>
   </div>
 </template>
 
