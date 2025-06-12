db7168ef94bdd8e2ac9d3fb6adf2c4649b2838f0
allworldg <944937881@qq.com>
2025-06-12 12:14:14 +0800

    add box-shadow

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="">
-    this is home page
+    <div
+      class="bg-[#5cb85c] shadow-[0px_8px_8px_-8px_inset_rgb(0,0,0,0.3),0px_-8px_8px_-8px_inset_rgb(0,0,0,0.3)]"
+    >
+      this is banner
+      <div>
+        <h1>Conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+    </div>
   </div>
 </template>
 
